d0caf22999b2f69f0412c9b7fae515589e015055
chivayVM <hubert.jasudowic@gmail.com>
2015-12-12 21:11:31 +0100

    VM fully functinal. Added comments

--- WPI_NOCVM/src/nocvm.c
+++ WPI_NOCVM/src/nocvm.c
@@ -2,10 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+/* Size of memory and word in memory */
 #define N 256
 
+/* Virtual address of AC register */
 #define AC_ADDRESS -1
 
+
+/* Operations codes available to use */
 typedef enum {
 	JSR = 0x00,		/* jump to subroutine */
 	JMP = 0x01,		/* jump */
@@ -41,6 +46,7 @@ typedef enum {
 	ONU = 0x1F		/* output number */
 } OPCODE;
 
+/* Types of addressing modes of operands */
 typedef enum {
 	acc = 0x00,		/* accumulator */
 	ind = 0x01,		/* indirect  */
@@ -52,6 +58,8 @@ typedef enum {
 	rel = 0x07       /* relative */
 } ADDRESS_MODE;
 
+
+/* Structure containing state of VM */
 typedef struct {
 	int AC;
 	int SP;
@@ -60,37 +68,38 @@ typedef struct {
 	int memory[N];
 
 	int program_length;
-
 } vm_state;
 
+
+/* Structure containing parameters of current instruction */
 typedef struct {
 	OPCODE opcode;
 	ADDRESS_MODE address_mode;
 
 	int operand_value;
 	int operand_address;
-
 } instruction;
 
-int mod(int w, int n)
+
+/* Computes computes math-like remainder of @w / @n, not the same as % */
+int modulo(int w, int n)
 {
 	return (w % n < 0) ? (w % n + n) : (w % n);
 }
 
+/* Computes modulo of @w by N */
+int mod(int w)
+{
+	return modulo(w, N);
+}
+
+/* Computes bitwise NAND of @a and @b */
 int nand(int a, int b)
 {
 	return ~(a & b);
 }
 
-void dump_memory(vm_state *vm)
-{
-	int i;
-	printf("[");
-	for(i = 0; i < N; i++)
-		printf("%02X ", vm->memory[i]);
-	printf("]\n");
-}
-
+/* Sets values of registers and memory to 0 */
 void reset_machine(vm_state *vm)
 {
 	vm->AC = 0;
@@ -101,76 +110,116 @@ void reset_machine(vm_state *vm)
 	memset(vm->memory, 0, sizeof(int)*N);
 }
 
-
+/* Gets next instruction to process from address pointed by IP */
 void fetch_instruction(vm_state *vm, instruction *dst)
 {
 	int instr = vm->memory[ vm->IP ];
-	dst->opcode       = instr >>   3;
-	dst->address_mode = instr  & 0x7;
+
+	dst->opcode       = instr >>   3; /* inst / 8 */
+	dst->address_mode = instr  & 0x7; /* inst % 8 */
 }
 
 /*******************************************************/
 /*               OPERAND FUNCTIONS                     */
+/*******************************************************/
 
+/* Handles addressing mode: acc
+ * operand: ACC register
+ * stores operand address and value in @inst
+ */
 void load_operand_acc(vm_state *vm, instruction *inst)
 {
 	inst->operand_address = AC_ADDRESS;
 	inst->operand_value = vm->AC;
 }
 
+/* Handles addressing mode: ind
+ * operand: word pointed by AC
+ * stores operand address and value in @inst
+ */
 void load_operand_ind(vm_state *vm, instruction *inst)
 {
 	inst->operand_address = vm->AC;
 	inst->operand_value   = vm->memory[ inst->operand_address ];
 }
 
+/* Handles addressing mode: pop
+ * operand: word pointed by SP 
+ * stores operand address and value in @inst, moves SP to next word
+ */
 void load_operand_pop(vm_state *vm, instruction *inst)
 {
 	inst->operand_address = vm->SP;	
 	inst->operand_value   = vm->memory[ inst->operand_address ];
 
-	vm->SP = mod(vm->SP + 1, N);
+	vm->SP = mod(vm->SP + 1);
 }
 
+/* Handles addressing mode: psh
+ * operand: word pointed by SP 
+ * moves SP to previous word, stores operand address and value in @inst
+ */
 void load_operand_psh(vm_state *vm, instruction *inst)
 {
-	vm->SP = mod(vm->SP - 1, N);
+	vm->SP = mod(vm->SP - 1);
 
 	inst->operand_address = vm->SP;
 	inst->operand_value   = vm->memory[ inst->operand_address ];
 }
 
+/* Handles addressing mode: imm
+ * operand: word pointed by IP 
+ * stores operand address and value in @inst
+ */
 void load_operand_imm(vm_state *vm, instruction *inst)
 {
 	inst->operand_address = vm->IP;
 	inst->operand_value   = vm->memory[ inst->operand_address ];
 
 	/* Skip next instruction */
-	vm->IP = mod(vm->IP + 1, N);	
+	vm->IP = mod(vm->IP + 1);	
 }
 
+/* Handles addressing mode: abs
+ * operand: word at address pointed by word pointed by IP
+ * stores operand address and value in @inst
+ */
 void load_operand_abs(vm_state *vm, instruction *inst)
 {
 	inst->operand_address = vm->memory[ vm->IP ];
 	inst->operand_value   = vm->memory[ inst->operand_address ];
 
-	vm->IP = mod(vm->IP + 1, N);	
+	/* Skip next instruction */
+	vm->IP = mod(vm->IP + 1);	
 }
 
+/* Handles addressing mode: dis
+ * operand: word at address = word poined by IP + SP
+ * stores operand address and value in @inst
+ */
 void load_operand_dis(vm_state *vm, instruction *inst)
 {
-	inst->operand_address = mod(vm->memory[ vm->IP ] + vm->SP, N);
+	inst->operand_address = mod(vm->memory[ vm->IP ] + vm->SP);
 	inst->operand_value   = vm->memory[ inst->operand_address ];
-	vm->IP = mod(vm->IP + 1, N);
+
+	/* Skip next instruction */
+	vm->IP = mod(vm->IP + 1);
 }
 
+/* Handles addressing mode: rel
+ * operand: word at address = word poined by IP + SP + 1
+ * stores operand address and value in @inst
+ */
 void load_operand_rel(vm_state *vm, instruction *inst)
 {
-	inst->operand_address = mod( vm->memory[ vm->IP ] + vm->IP + 1, N);
+	inst->operand_address = mod(vm->memory[ vm->IP ] + vm->IP + 1);
 	inst->operand_value   = vm->memory[ inst->operand_address ];
-	vm->IP = mod(vm->IP + 1, N);
+
+	/* Skip next instruction */
+	vm->IP = mod(vm->IP + 1);
 }
 
+/* Runs function responsible for loading operand for current instrution */
 void load_operand(vm_state *vm, instruction *inst)
 {
 	switch (inst->address_mode) {
@@ -211,13 +260,25 @@ void load_operand(vm_state *vm, instruction *inst)
 	}
 }
 
-/*        END OF OPERAND FUNCTIONS                     */
+/*******************************************************/
+/*            END OF OPERAND FUNCTIONS                 */
 /*******************************************************/
 
 
 /*******************************************************/
 /*                OPCODE FUNCTIONS                     */
+/*******************************************************/
 
+/* Saves mod( @value ) in address @op_adress,  */
+void save_to_operand(vm_state *vm, int op_address, int value)
+{
+	if (op_address == AC_ADDRESS)
+		vm->AC = mod(value);
+	else
+		vm->memory[op_address] = mod(value);
+}
+
+/* Handles opcode: JSR */
 void op_jsr(vm_state *vm, instruction *inst)
 {
 	if(inst->operand_address == AC_ADDRESS)
@@ -227,13 +288,16 @@ void op_jsr(vm_state *vm, instruction *inst)
 	vm->IP = inst->operand_address;
 }
 
+/* Handles opcode: JMP */
 void op_jmp(vm_state *vm, instruction *inst)
 {
 	if(inst->operand_address == AC_ADDRESS)
 		exit(EXIT_SUCCESS);
+
 	vm->IP = inst->operand_address;
 }
 
+/* Handles opcode: JPZ */
 void op_jpz(vm_state *vm, instruction *inst)
 {
 	if(inst->operand_address == AC_ADDRESS)
@@ -243,6 +307,7 @@ void op_jpz(vm_state *vm, instruction *inst)
 		vm->IP = inst->operand_address;
 }
 
+/* Handles opcode: JNZ */
 void op_jnz(vm_state *vm, instruction *inst)
 {
 	if(inst->operand_address == AC_ADDRESS)
@@ -252,148 +317,162 @@ void op_jnz(vm_state *vm, instruction *inst)
 		vm->IP = inst->operand_address;
 }
 
+/* Handles opcode: NND */
 void op_nnd(vm_state *vm, instruction *inst)
 {
 	vm->AC = nand(vm->AC, inst->operand_value);
 }
 
+/* Handles opcode: DNN */
 void op_dnn(vm_state *vm, instruction *inst)
 {
-	if(inst->operand_address == AC_ADDRESS)
-		vm->AC = nand(vm->AC, inst->operand_value);
-	else
-		vm->memory[ inst->operand_address ] = nand(vm->AC, inst->operand_value);
+	int res = nand(vm->AC, inst->operand_value);
+	save_to_operand(vm, inst->operand_address, res);
 }
 
+/* Handles opcode: INC */
 void op_inc(vm_state *vm, instruction *inst)
 {
-	if(inst->operand_address == AC_ADDRESS)
-		vm->AC++;
-	else
-		vm->memory[ inst->operand_address ]++;
+	int res = inst->operand_value + 1;
+	save_to_operand(vm, inst->operand_address, res);
 }
 
+/* Handles opcode: DEC */
 void op_dec(vm_state *vm, instruction *inst)
 {
-	if(inst->operand_address == AC_ADDRESS)
-		vm->AC--;
-	else
-		vm->memory[ inst->operand_address ]--;
+	int res = inst->operand_value - 1;
+	save_to_operand(vm, inst->operand_address, res);
 }
 
+/* Handles opcode: DDA */
 void op_dda(vm_state *vm, instruction *inst)
 {
-	if(inst->operand_address == AC_ADDRESS)
-		vm->AC = vm->AC + inst->operand_value;
-	else
-		vm->memory[ inst->operand_address ] = vm->AC + inst->operand_value;
+	int res = vm->AC + inst->operand_value;
+	save_to_operand(vm, inst->operand_address, res);
 }
 
+/* Handles opcode: BUS */
 void op_bus(vm_state *vm, instruction *inst)
 {
-	if(inst->operand_address == AC_ADDRESS)
-		vm->AC = inst->operand_value -  vm->AC;
-	else
-		vm->memory[ inst->operand_address ] =  inst->operand_value - vm->AC;	
+	int res = inst->operand_value - vm->AC;
+	save_to_operand(vm, inst->operand_address, res);	
 }
 
+/* Handles opcode: LUM */
 void op_lum(vm_state *vm, instruction *inst)
 {
-	if(inst->operand_address == AC_ADDRESS)
-		vm->AC = vm->AC * inst->operand_value;
-	else
-		vm->memory[ inst->operand_address ] = vm->AC * inst->operand_value;
+	int res = inst->operand_value * vm->AC;
+	save_to_operand(vm, inst->operand_address, res);
 }
 
+/* Handles opcode: VID */
 void op_vid(vm_state *vm, instruction *inst)
 {
+	int res;
 	if(vm->AC == 0)
 		exit(EXIT_FAILURE);
 
-	if(inst->operand_address == AC_ADDRESS)
-		vm->AC = inst->operand_value / vm->AC;
-	else
-		vm->memory[ inst->operand_address ] = inst->operand_value / vm->AC;
+	res = inst->operand_value / vm->AC;
+	save_to_operand(vm, inst->operand_address, res);
 }
 
+/* Handles opcode: DOM */
 void op_dom(vm_state *vm, instruction *inst)
 {
+	int res;
 	if(vm->AC == 0)
 		exit(EXIT_FAILURE);
 
-	if(inst->operand_address == AC_ADDRESS)
-		vm->AC = inst->operand_value % vm->AC;
-	else
-		vm->memory[ inst->operand_address ] = inst->operand_value % vm->AC;	
+	res = inst->operand_value % vm->AC;
+	save_to_operand(vm, inst->operand_address, res);	
 }
 
+/* Handles opcode: SNE */
 void op_sne(vm_state *vm, instruction *inst)
 {
 	if(vm->AC != inst->operand_value)
-		vm->IP = mod(vm->IP + 2, N);
+		vm->IP = mod(vm->IP + 2);
 }
 
+/* Handles opcode: SGE */
 void op_sge(vm_state *vm, instruction *inst)
 {
 	if(vm->AC >= inst->operand_value)
-		vm->IP = mod(vm->IP + 2, N);
+		vm->IP = mod(vm->IP + 2);
 }
 
+/* Handles opcode: SLE */
 void op_sle(vm_state *vm, instruction *inst)
 {
 	if(vm->AC <= inst->operand_value)
-		vm->IP = mod(vm->IP + 2, N);
+		vm->IP = mod(vm->IP + 2);
 }
 
+/* Handles opcode: ADD */
 void op_add(vm_state *vm, instruction *inst)
 {
-	vm->AC += inst->operand_value;
+	int res = vm->AC + inst->operand_value;
+	save_to_operand(vm, AC_ADDRESS, res);
 }
 
+/* Handles opcode: SUB */
 void op_sub(vm_state *vm, instruction *inst)
 {
-	vm->AC -= inst->operand_value;
+	int res = vm->AC - inst->operand_value;
+	save_to_operand(vm, AC_ADDRESS, res);
 }
 
+/* Handles opcode: MUL */
 void op_mul(vm_state *vm, instruction *inst)
 {
-	vm->AC *= inst->operand_value;
+	int res = vm->AC * inst->operand_value;
+	save_to_operand(vm, AC_ADDRESS, res);
 }
 
+/* Handles opcode: DIV */
 void op_div(vm_state *vm, instruction *inst)
 {
+	int res;
 	if(inst->operand_value == 0)
 		exit(EXIT_FAILURE);
 
-	vm->AC /= inst->operand_value;
+	res = vm->AC / inst->operand_value;
+	save_to_operand(vm, AC_ADDRESS, res);
 }
 
+/* Handles opcode: MOD */
 void op_mod(vm_state *vm, instruction *inst)
 {
+	int res;
 	if(inst->operand_value == 0)
 		exit(EXIT_FAILURE);
 
-	vm->AC %= inst->operand_value;	
+	res = vm->AC % inst->operand_value;
+	save_to_operand(vm, AC_ADDRESS, res);
 }
 
+/* Handles opcode: SEQ */
 void op_seq(vm_state *vm, instruction *inst)
 {
 	if(vm->AC == inst->operand_value)
-		vm->IP = mod(vm->IP + 2, N);
+		vm->IP = mod(vm->IP + 2);
 }
 
+/* Handles opcode: SLT */
 void op_slt(vm_state *vm, instruction *inst)
 {
 	if(vm->AC < inst->operand_value)
-		vm->IP = mod(vm->IP + 2, N);
+		vm->IP = mod(vm->IP + 2);
 }
 
+/* Handles opcode: SGT */
 void op_sgt(vm_state *vm, instruction *inst)
 {
 	if(vm->AC > inst->operand_value)
-		vm->IP = mod(vm->IP + 2, N);	
+		vm->IP = mod(vm->IP + 2);	
 }
 
+/* Handles opcode: LAA */
 void op_laa(vm_state *vm, instruction *inst)
 {
 	if(inst->operand_address == AC_ADDRESS)
@@ -402,6 +481,7 @@ void op_laa(vm_state *vm, instruction *inst)
 	vm->AC = inst->operand_address;
 }
 
+/* Handles opcode: LAS */
 void op_las(vm_state *vm, instruction *inst)
 {
 	if(inst->operand_address == AC_ADDRESS)
@@ -410,17 +490,20 @@ void op_las(vm_state *vm, instruction *inst)
 	vm->SP = inst->operand_address;
 }
 
+/* Handles opcode: LDA */
 void op_lda(vm_state *vm, instruction *inst)
 {
 	vm->AC = inst->operand_value;
 }
 
+/* Handles opcode: STA */
 void op_sta(vm_state *vm, instruction *inst)
 {
 	if(inst->operand_address != AC_ADDRESS)
 		vm->memory[ inst->operand_address ] = vm->AC;
 }
 
+/* Handles opcode: ICH */
 void op_ich(vm_state *vm, instruction *inst)
 {
 	int x = getchar();
@@ -434,11 +517,13 @@ void op_ich(vm_state *vm, instruction *inst)
 		vm->memory[ inst->operand_address ] = x;
 }
 
+/* Handles opcode: OCH */
 void op_och(instruction *inst)
 {
 	putchar(inst->operand_value);
 }
 
+/* Handles opcode: INU */
 void op_inu(vm_state *vm, instruction *inst)
 {
 	int x;
@@ -451,18 +536,19 @@ void op_inu(vm_state *vm, instruction *inst)
 		vm->AC = x;
 	else
 		vm->memory[ inst->operand_address ] = x;
-
 }
 
+/* Handles opcode: ONU */
 void op_onu(instruction *inst)
 {
 	printf("%d", inst->operand_value);
 }
 
+/*******************************************************/
 /*               END OF OPCODE FUNCTIONS               */
 /*******************************************************/
 
-
+/* Executes instruction described in @inst based on opcode */
 void execute_instruction(vm_state* vm, instruction *inst)
 {
 	switch (inst->opcode) {
@@ -597,17 +683,16 @@ void execute_instruction(vm_state* vm, instruction *inst)
 		default:
 			exit(EXIT_FAILURE);
 	}
-
 }
 
+/* Main loop of VM */
 void run(vm_state *vm)
 {
 	instruction curr_instr;
 
 	while(vm->IP < vm->program_length) {
-
 		fetch_instruction(vm, &curr_instr);
-		vm->IP = mod(vm->IP + 1, N);
+		vm->IP = mod(vm->IP + 1);
 		load_operand(vm, &curr_instr);
 		execute_instruction(vm, &curr_instr);
 	}
