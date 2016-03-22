a8f2f13f7ebc33903336b1c7e489a21cd20a52b5
chivayVM <hubert.jasudowicz@gmail.com>
2016-03-22 23:36:55 +0100

    Fix

--- IPP_2016/IPP_HOSPITAL/test.sh
+++ IPP_2016/IPP_HOSPITAL/test.sh
@@ -7,8 +7,14 @@
 
 if [ $# -lt 2 ]
 then
-	echo "USAGE: $0 prog directory";
+	echo "USAGE: $0 (-v) prog directory";
 	exit;
+fi
+
+if [ "$1" = "-v" ];
+then
+	verbose=true
+	shift
 fi
 
 INPUT=$2"/*.in"
@@ -16,10 +22,31 @@ INPUT=$2"/*.in"
 for input in $INPUT
 do
 	echo -n "$input: ";
-	if $1 < $input | diff - ${input%in}out  >/dev/null 2>&1;
-	then 
-		echo OK;
+	if [ "$verbose" = "true" ];
+	then
+		$1 -v < $input >log.out 2>log_err.out
 	else
-		echo Źle!;
+		$1 < $input >log.out 2>log_err.out
 	fi
+
+	if diff log.out ${input%in}out  >/dev/null 2>&1;
+	then 
+		echo -n "STDOUT:OK ";
+	else
+		echo -n "STDOUT:Zle! ";
+	fi
+
+	if [ "$verbose" = "true" ];
+	then
+		if diff log_err.out ${input%in}err  >/dev/null 2>&1;
+		then 
+			echo -n "STDERR:OK";
+		else
+			echo -n "STDERR:Zle!";
+		fi
+	fi
+	echo
+
 done
+
+rm log.out log_err.out > /dev/null
