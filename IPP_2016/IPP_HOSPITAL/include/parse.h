10817d043c367ffba83a13c967f4ff2fda87d29a
chivay <hubert.jasudowicz@gmail.com>
2016-03-23 21:30:35 +0100

    Cosmetic fixes

--- IPP_2016/IPP_HOSPITAL/include/parse.h
+++ IPP_2016/IPP_HOSPITAL/include/parse.h
@@ -16,28 +16,36 @@
 #define STATUS_FAIL "IGNORED"
 #define STATUS_SUCCESS "OK"
 
-enum COMMAND_TYPE
-{ 
-	ND_ENTER,  // NEW_DISEASE_ENTER_DESCRIPTION name disease description
-	ND_COPY,   // NEW_DISEASE_COPY_DESCRIPTION name1 name2
-	CHG_DESC,  // CHANGE_DESCRIPTION name n disease description
-	PRNT_DESC, // PRINT_DESCRIPTION name n
-	DEL_PAT,   // DELETE_PATIENT_DATA name 
-	ERR
+enum COMMAND_TYPE {
+    ND_ENTER,  // NEW_DISEASE_ENTER_DESCRIPTION name disease description
+    ND_COPY,   // NEW_DISEASE_COPY_DESCRIPTION name1 name2
+    CHG_DESC,  // CHANGE_DESCRIPTION name n disease description
+    PRNT_DESC, // PRINT_DESCRIPTION name n
+    DEL_PAT,   // DELETE_PATIENT_DATA name
+    ERR
 };
 
 typedef enum COMMAND_TYPE commandType;
 
-char* readToWhitespace();
-char* readToEndline();
+// Read next token to first whitespace
+char *readToWhitespace();
 
+// Read next token until \n
+char *readToEndline();
+
+// Initialize strtok and get type of command
 commandType getCommand(char *line);
+
+// Read line of STDIN and parse command and arguments
 commandType readCommand(char **arg1, char **arg2, char **arg3);
 
-bool readParameters(Hospital *hosp, int argc, char  **argv);
-void readInput(Hospital *hosp);
+// Parse arguments used in running the executable
+bool readParameters(Hospital *hosp, int argc, char **argv);
 
+// Print status of command
 void reportStatus(const char *status);
+
+// Print number of descriptions in memory
 void reportDescriptions(int cnt);
 
 #endif
