e83a96bc748474643c1de85d62f614ea635aa232
chivayVM <hubert.jasudowicz@gmail.com>
2016-03-31 16:02:58 +0200

    Modfied additional scripts

--- IPP_2016/IPP_HOSPITAL/test.sh
+++ IPP_2016/IPP_HOSPITAL/test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#USAGE ./test.sh prog directory
 
+# Running without arguments
 if [ $# -lt 2 ]
 then
 	echo "USAGE: $0 (-v) prog directory";
 	exit;
 fi
 
+# Check verbosity
 if [ "$1" = "-v" ];
 then
 	verbose=true
@@ -17,32 +18,28 @@ INPUT=$2"/*.in"
 
 for input in $INPUT
 do
-	echo -n "$input: ";
-	if [ "$verbose" = "true" ];
-	then
+	echo "Checking $input...";
+
+	# Run test
+	if [ "$verbose" = "true" ]; then
 		$1 -v < $input >log.out 2>log_err.out
 	else
 		$1 < $input >log.out 2>log_err.out
 	fi
 
-	if diff log.out ${input%in}out  >/dev/null 2>&1;
-	then 
-		echo -n "STDOUT:OK ";
-	else
-		echo -n "STDOUT:Zle! ";
+	# Check STDOUT
+	if ! diff log.out ${input%in}out  >/dev/null 2>&1; then
+		echo "STDOUT: ERROR!";
 	fi
 
-	if [ "$verbose" = "true" ];
-	then
-		if diff log_err.out ${input%in}err  >/dev/null 2>&1;
-		then 
-			echo -n "STDERR:OK";
-		else
-			echo -n "STDERR:Zle!";
+	# If verbose, check STDERR
+	if [ "$verbose" = "true" ]; then
+		if ! diff log_err.out ${input%in}err  >/dev/null 2>&1; then
+			echo "STDERR: ERROR!";
 		fi
 	fi
-	echo
 
 done
 
+# Remove temporary files
 rm log.out log_err.out > /dev/null
